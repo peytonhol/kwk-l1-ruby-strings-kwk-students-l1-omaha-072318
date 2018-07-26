29ec9b58103fb87178047d37d2047e3af49c054b
peytonhol <peyhol@yahoo.com>
2018-07-26 14:32:26 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,13 @@
-# Code your prompts here!
+def invitation{#guest_name, #party_name, #date, #time, #host_name}
+  puts "Dear #{guest_name},"
+  gets #{guest_name}
+  puts "You are cordially invited to #{party_name} on #{date} at #{time}pm. Please RSVP no later than October 30."
 
-# Try starting out with puts'ing a string.
+  puts "Sincerely,"
+  
+  puts "#{host_name}
+
+end(Hermoine, the Best Halloween Party Ever, October 31, 6)
+
+invitation
+
